b0247c9e0d2e9bc7cbd3cc0d3b70591586defc3c
evgeniy-kulikov <redstepway@gmail.com>
2024-02-27 10:15:08 +0300

    hr.momels.Contact

--- django_orm/settings.py
+++ django_orm/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "hr.apps.HrConfig",
+
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- hr/admin.py
+++ hr/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from hr.models import Employee, Contact
 
-# Register your models here.
+admin.site.register(Contact)
+
+admin.site.register(Employee)
+
